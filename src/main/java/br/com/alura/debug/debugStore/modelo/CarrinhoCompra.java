59ad8cbe75730c061ab0bc843ce39a190d0f3e4f
Raimundo Martins Pinto Neto <rmpn.pinto@gmail.com>
2019-07-05 14:42:14 -0300

    Produto pessoa jurídica

--- src/main/java/br/com/alura/debug/debugStore/modelo/CarrinhoCompra.java
+++ src/main/java/br/com/alura/debug/debugStore/modelo/CarrinhoCompra.java
@@ -4,28 +4,43 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class CarrinhoCompra {
-	
+
 	private FormaPagamento formaPagamento;
-	
+
 	public CarrinhoCompra(FormaPagamento formaPagamento) {
 		this.formaPagamento = formaPagamento;
 	}
-	
+
 	public FormaPagamento getFormaPagamento() {
 		return formaPagamento;
 	}
-	
+
 	public BigDecimal calcularPrecoFinal(List<Produto> produtos) {
 		BigDecimal total = new BigDecimal(0);
 		for (Produto produto : produtos) {
 			BigDecimal preco = produto.getPreco();
 			double porcentagemDesconto = this.getFormaPagamento().getPorcentagemDesconto();
 			BigDecimal valorComDesconto = preco.multiply(new BigDecimal(porcentagemDesconto)).divide(new BigDecimal("100"));
-			BigDecimal valorProdutoComDesconto =  preco.subtract(valorComDesconto);
-			total = total.add(valorProdutoComDesconto);
+			BigDecimal valorProdutoComDesconto = preco.subtract(valorComDesconto);
+			if (valorProdutoComDesconto.compareTo(new BigDecimal("700.00")) == 1) {
+				System.out.println("O produto " + produto.getDescricao() + " ganhou mais R$20.00 de desconto");
+				valorProdutoComDesconto = valorProdutoComDesconto.subtract(new BigDecimal("20.00"));
+			}
+
+			BigDecimal multiplicador = new BigDecimal(produto.getQuantidade());
+			total = total.add(valorProdutoComDesconto.multiply(multiplicador));
+		}
+		return total;
+	}
+	
+	private BigDecimal calcularPromocao(List<Produto> produtos, BigDecimal total) {
+		
+		for (Produto produto : produtos) {
+			if(produto.getQuantidade() > 3) {
+				total = total.subtract(produto.getPreco());
+			}
 		}
 		return total;
 	}
 
-	
 }
