944040b6fd2b6f9da7c8ff2926e815e77a0ae6be
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-07-05 22:44:51 -0300

    ajuste da Junit

--- src/main/java/br/com/alura/debug/debugStore/CompraPessoaJuridica.java
+++ src/main/java/br/com/alura/debug/debugStore/CompraPessoaJuridica.java
@@ -10,7 +10,7 @@ import br.com.alura.debug.debugStore.modelo.FormaPagamento;
 import br.com.alura.debug.debugStore.modelo.Produto;
 
 public class CompraPessoaJuridica {
-	
+
 	public static void main(String[] args) {
 		Categoria perfumaria = new Categoria("Perfumaria");
 		List<Produto> produtos = criarProdutos(perfumaria);
@@ -19,21 +19,18 @@ public class CompraPessoaJuridica {
 		System.out.println("Valor da compra: " + valorFinalDaCompra);
 	}
 
-	
-
 	private static List<Produto> criarProdutos(Categoria categoria) {
-		
-		//5 Mulheres na equipe
-		Produto _212 = new Produto("212 Sexy Feminino",new BigDecimal(("179.99")), categoria, 5);
 
-		//2 Diretoras na equipe
+		// 5 Mulheres na equipe
+		Produto _212 = new Produto("212 Sexy Feminino", new BigDecimal(("179.99")), categoria, 5);
+
+		// 2 Diretoras na equipe
 		Produto chanel = new Produto("Chanel Nº5 ", new BigDecimal("665"), categoria, 2);
-		
-		
-		//7 Homens da equipe
+
+		// 7 Homens da equipe
 		Produto sauvage = new Produto("Sauvage", new BigDecimal("375"), categoria, 7);
-		
-		return  Arrays.asList(_212, chanel, sauvage);
+
+		return Arrays.asList(_212, chanel, sauvage);
 	}
-	
+
 }
